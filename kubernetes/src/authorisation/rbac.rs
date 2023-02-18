988e667dadf386f59f106cfbebd04b35fa47f261
root <rnarkkx@gmail.com>
2023-02-18 09:48:32 +0000

    Update

--- kubernetes/src/authorisation/rbac.rs
+++ kubernetes/src/authorisation/rbac.rs
@@ -18,24 +18,41 @@ pub use role_binding::RoleBinding;
 //     RoleBinding(RoleBinding)
 // }
 
+/// RoleRef contains information that points to the role being used.
 #[derive(Debug, Decode)]
 pub struct RoleRef {
+    /// Group for the resource being referenced.
     api_group: String,
+    /// Kind of resource being referenced.
     kind: String,
+    /// Name of resource being referenced.
     name: String
 }
 
+/// Subject contains a reference to the object or user identities a role binding applies to. This can either hold a direct API object reference, or a value for non-objects such as user and group names.
 #[derive(Debug, Decode)]
-pub struct Subject {
-    kind: SubjectKind,
-    name: String,
-    api_group: Option<String>,
-    namespace: Option<String>,
-}
-
-#[derive(Debug, Decode)]
-pub enum SubjectKind {
-    User,
-    Group,
-    ServiceAccount
+pub enum Subject {
+    User {
+        #[kfl(argument)]
+        name: String,
+        /// API group of the referenced subject. Defaults to "rbac.authorization.k8s.io".
+        #[kfl(property, default = "rbac.authorization.k8s.io".into())]
+        api_group: String,
+    },
+    Group {
+        #[kfl(argument)]
+        name: String,
+        /// API group of the referenced subject. Defaults to "rbac.authorization.k8s.io".
+        #[kfl(property, default = "rbac.authorization.k8s.io".into())]
+        api_group: String,
+    },
+    ServiceAccount {
+        #[kfl(argument)]
+        name: String,
+        /// API group of the referenced subject. Defaults to "".
+        #[kfl(property, default)]
+        api_group: Option<String>,
+        /// Namespace of the referenced object.
+        namespace: Option<String>,
+    }
 }
--- kubernetes/src/authorisation/rbac/cluster_role_binding.rs
+++ kubernetes/src/authorisation/rbac/cluster_role_binding.rs
@@ -9,6 +9,8 @@ use super::{RoleRef, Subject};
 #[derive(Debug, Decode)]
 pub struct ClusterRoleBinding {
     metadata: Option<Metadata>,
+    /// RoleRef can only reference a ClusterRole in the global namespace. If the RoleRef cannot be resolved, the Authorizer must return an error.
     role_ref: RoleRef,
+    /// Subjects holds references to the objects the role applies to.
     subjects: Vec<Subject>
 }
